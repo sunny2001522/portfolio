e4fe9e257159aa083aa9623880a2f79e39504a5b
sunny2001522 <seliya512399@gmail.com>
2025-09-24 21:37:01 +0800

    feat: create FrontEndPage component and remove locale handling from HomePage

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,16 +1,10 @@
 import { getTranslations } from "next-intl/server";
-import { setRequestLocale } from "next-intl/server";
 
 export default async function HomePage({
   params,
 }: {
   params: Promise<{ locale: string }>;
 }) {
-  const { locale } = await params;
-
-  // 啟用靜態渲染
-  setRequestLocale(locale);
-
   // 在服務器組件中使用 getTranslations 而不是 useTranslations
   const t = await getTranslations("HomePage");
 
