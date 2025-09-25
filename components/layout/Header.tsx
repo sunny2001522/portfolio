37f0eb739d01e3553f77468eaf00f0cd3639b4ee
sunny2001522 <seliya512399@gmail.com>
2025-09-25 08:56:55 +0800

    feat:add header content

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,13 +1,14 @@
 import { getTranslations } from "next-intl/server";
+import LanguageSwitcher from "./LanguageSwitcher";
 
 // app/[locale]/frontend/page.tsx
 export default async function Header() {
   const t = await getTranslations("HomePage");
 
   return (
-    <div className="flex w-full">
+    <div className="flex w-full justify-between max-w-330 mx-auto py-3">
       <h1>{t("title")}</h1>
-      <p>這是 FE 頁面</p>
+      <LanguageSwitcher />
     </div>
   );
 }
