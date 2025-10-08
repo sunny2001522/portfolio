82d94d1f360e28d08cc3924b0b81b6a1d07d094e
sunny2001522 <seliya512399@gmail.com>
2025-10-08 22:32:58 +0800

    feat: refactor Introduction and Web components for improved layout and functionality

--- components/page/role/Introduction.tsx
+++ components/page/role/Introduction.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import Web from "./Web";
-import Link from "next/link";
+// import Link from "next/link";
 
 import { useLocale, useTranslations } from "next-intl";
 import Bubble from "@/components/ui/Bubble";
-import { Button } from "@/components/ui/button";
-import { FaFileDownload } from "react-icons/fa";
+// import { Button } from "@/components/ui/button";
+// import { FaFileDownload } from "react-icons/fa";
 
 export default function Introduction({ role }: { role: string }) {
   const t = useTranslations(role);
@@ -13,39 +13,38 @@ export default function Introduction({ role }: { role: string }) {
 
   return (
     <div className="relative w-full h-full snap-center snap-always  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 items-center justify-center">
-      <div className="absolute top-1/3 left-1/3 -translate-x-1/2 -translate-y-1/2 z-50 text-wrap w-1/3">
+      <div className="absolute top-1/3 left-1/3 -translate-x-1/2 -translate-y-1/2 z-50 text-wrap w-1/2 md:w-1/3">
         <h2>{t("title")}</h2>
         <p>{t("me")}</p>
         <ul className="text-sm">
           <li>{t("about.skill")}</li>
           <li>{t("about.description")}</li>
         </ul>
-        <Button
+        {/* <Button
           asChild
-          className="bg-black text-white hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200"
+          className="bg-black text-white hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200 z-50"
         >
           <Link href={t("resume")} download={t("resume")}>
             <FaFileDownload className="mr-2" />
             Resume
           </Link>
-        </Button>
+        </Button> */}
       </div>
 
       {/* 裝飾 */}
-      <div className="absolute w-1/4 z-5 rotate-3 bottom-1/5 right-1/16">
-        <Web />
+      <div className="absolute w-2/3 md:w-1/3 lg:w-1/4 z-40 rotate-3 bottom-1/5 right-1/16">
+        <Web role={role as "fe" | "pm" | "ui" | "design"} isActive={true} />
       </div>
 
-      <div className="w-96 h-96 z-50 absolute bottom-0">
+      <div className="w-80 h-80 z-10 absolute bottom-1/2 translate-y-1/2 right-10 hidden md:block">
         {" "}
-        {/* 設定你想要的大小 */}
         <Bubble />
       </div>
 
       <Image
         src="/shape/shape10.webp"
         alt="decoration 2"
-        className="absolute right-1/4 top-20 z-10 w-40 animate-spin"
+        className="absolute right-1/10 top-2 z-0 w-40 animate-spin"
         unoptimized
         width={32}
         height={32}
@@ -54,15 +53,15 @@ export default function Introduction({ role }: { role: string }) {
       <Image
         src="/shape/shape16.webp"
         alt="decoration 2"
-        className="absolute left-0 top-20 z-10 "
+        className="absolute bottom-1/5 z-0 -left-1/10 translate-y-1/2 "
         unoptimized
         width={700}
         height={700}
       />
       <Image
-        src="/shape/shape3.webp"
+        src="/shape/shape13.webp"
         alt="decoration 3"
-        className="absolute left-10 bottom-1/2 z-10 w-40"
+        className="absolute left-10 top-0 z-0 w-30"
         unoptimized
         width={32}
         height={32}
@@ -70,7 +69,7 @@ export default function Introduction({ role }: { role: string }) {
       <Image
         src="/shape/shape4.webp"
         alt="decoration 4"
-        className="absolute right-10 top-1/3 z-0 w-40"
+        className="absolute right-10 top-1/3 z-0 w-40 hidden md:block"
         unoptimized
         width={32}
         height={32}
@@ -78,7 +77,7 @@ export default function Introduction({ role }: { role: string }) {
       <Image
         src="/shape/shape14.webp"
         alt="liquid background"
-        className="absolute top-0 z-0 "
+        className="absolute top-0 -left-1/3 md:-left-1/10 z-0 "
         unoptimized
         width={300}
         height={32}
@@ -86,7 +85,7 @@ export default function Introduction({ role }: { role: string }) {
       <Image
         src="/shape/shape17.webp"
         alt="decoration 44"
-        className="absolute right-0 top-0 z-0 h-dvh"
+        className="absolute -right-1/2 md:-right-1/3 scale-50 md:scale-100 lg:right-0 top-0 z-0 "
         unoptimized
         width={600}
         height={2400}
@@ -94,7 +93,7 @@ export default function Introduction({ role }: { role: string }) {
       <Image
         src="/shape/shape12.webp"
         alt="decoration 1"
-        className="absolute left-3/4 top-20 z-0 w-40"
+        className="absolute left-3/4 top-20 z-0 w-40 hidden md:block"
         unoptimized
         width={32}
         height={32}
@@ -102,7 +101,7 @@ export default function Introduction({ role }: { role: string }) {
       <Image
         src="/shape/shape18.webp"
         alt="decoration 2"
-        className="absolute left-1/10 bottom-1/4 z-10 "
+        className="absolute left-1/10 bottom-1/4 z-0 hidden md:block"
         unoptimized
         width={200}
         height={200}
--- components/page/role/Web.tsx
+++ components/page/role/Web.tsx
@@ -1,53 +1,87 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import { Autoplay, Pagination } from "swiper/modules";
+import { useTranslations } from "next-intl";
 
-// 改為物件陣列，包含 title、description、color
-const slides = [
-  {
-    img: "/web/web1.webp",
-    title: "品牌形象",
-    description: "捕捉人物情感與故事的瞬間。",
-  },
-  {
-    img: "/web/web2.webp",
-    title: "風景攝影",
-    description: "展現大自然壯麗與細膩。",
-  },
-  {
-    img: "/web/web3.png",
-    title: "風景攝影",
-    description: "展現大自然壯麗與細膩。",
-  },
-];
+type Role = "fe" | "pm" | "ui" | "design";
 
-const Web = () => {
+interface WebProps {
+  role: Role;
+  isActive: boolean;
+}
+
+const Web = ({ role, isActive }: WebProps) => {
+  const t = useTranslations();
+
+  const staticImageMap: Record<Role, string> = {
+    ui: "/web/metro-app.jpg",
+    pm: "/web/gamified-weight-loss.webp",
+    fe: "/web/hotel-website.webp",
+    design: "/design/iceverseProduct.webp",
+  };
+
+  const projects = t.raw("projects") as Record<string, any>;
+
+  const imageMap: Record<string, string> = {
+    "gamified-weight-loss": "/web/gamified-weight-loss.webp",
+    "hotel-website": "/web/hotel-website.webp",
+    portfolio: "/web/portfolio.webp",
+    "metro-app": "/web/metro-app.webp",
+    book: "/design/book.webp",
+    iceverse: "/design/iceverse.webp",
+    iceverseProduct: "/design/iceverseProduct.webp",
+    actor: "/design/actor.webp",
+    taipei: "/design/taipei.webp",
+  };
+
+  const slides = Object.keys(projects)
+    .filter((projectId) => projects[projectId][role])
+    .map((projectId) => ({
+      img: imageMap[projectId],
+      title: projects[projectId].title,
+      description: projects[projectId].overview?.goal,
+    }));
+
   return (
-    <div className="flex flex-col items-center justify-center  font-bold  bg-white/50 shadow-md  backdrop-blur rounded-lg z-50 border-blue-200 border-2 overflow-hidden">
-      <div className="flex  gap-2 bg-gradient-to-r from-cyan-500 to-violet-200 px-4 py-2  w-full rounded-t-lg">
+    <div className="flex flex-col items-center justify-center font-bold bg-white/50 shadow-md backdrop-blur rounded-lg z-50 border-blue-200 border-2 overflow-hidden">
+      <div className="flex gap-2 bg-gradient-to-r from-cyan-500 to-violet-200 px-4 py-2 w-full ">
         <div className="rounded-full bg-white w-3 h-3"></div>
         <div className="rounded-full bg-white w-3 h-3"></div>
         <div className="rounded-full bg-white w-3 h-3"></div>
       </div>
-      <Swiper
-        direction={"vertical"}
-        loop={true}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        modules={[Autoplay, Pagination]}
-        grabCursor={true}
-        className=" aspect-video"
-      >
-        {slides.map((slide, idx) => (
-          <SwiperSlide key={idx} className={``}>
-            <div className=" flex flex-col shadow-md  h-full items-center justify-center   overflow-hidden">
-              <img src={slide.img} alt={slide.title} className="h-full " />
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      {isActive ? (
+        <Swiper
+          direction={"vertical"}
+          loop={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          modules={[Autoplay, Pagination]}
+          grabCursor={true}
+          className="aspect-video"
+        >
+          {slides.map((slide, idx) => (
+            <SwiperSlide key={idx}>
+              <div className="flex flex-col shadow-md h-full items-center justify-center overflow-hidden">
+                <img
+                  src={slide.img}
+                  alt={slide.title}
+                  className="h-full w-full object-cover"
+                />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      ) : (
+        <div className="aspect-video flex items-center justify-center overflow-hidden">
+          <img
+            src={staticImageMap[role]}
+            alt={role}
+            className="w-full h-30 object-cover aspect-video"
+          />
+        </div>
+      )}
     </div>
   );
 };
